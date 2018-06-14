755e8620242334fc4dfe05bd1c4d1f389ef8fa60
Dane <Dane@Danes-Mac-mini.lan>
2018-06-14 16:27:23 +0930

    New

---
+++ Random Number Generator/random.js
@@ -0,0 +1,18 @@
+var min = parseInt(prompt("please enter a random number:"));
+
+var max = parseInt(prompt("please enter a second random number:"));
+
+
+console.log(min, max);
+
+function randomiser(min, max) {
+    
+   return Math.floor( Math.random() * (max - min) + min);
+  
+}
+
+var randNum = randomiser(min, max); 
+
+console.log(randNum);
+
+alert("your random number is " + randNum);
